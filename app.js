accaf9b19b3be04de2c5282480da62c43e111a5b
Zac Hanson <zacthepk@gmail.com>
2018-10-19 04:04:51 +0000

    modified app.js

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 var bodyParser = require("body-parser"),
-methodOverride = require("method-override"),
 expressSanitizer = require("express-sanitizer"),
-mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
-app.use(methodOverride("_method"));
+
 
 
 
